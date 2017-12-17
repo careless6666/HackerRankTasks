614d3dab2fc6c7779646f6b490c3479dbb46d989
alex usov <careless6666@gmail.com>
2017-12-17 23:16:22 +0300

    added pairs diff task

---
+++ HackerRank/Medium/PairsWithDiff.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank.Medium
+{
+    //https://www.hackerrank.com/challenges/pairs/problem
+    public class PairsWithDiff
+    {
+        public static int Get(int[] a, int k)
+        {
+            var counter = 0;
+
+            for (var i = 0; i < a.Length; i++)
+            {
+                for (var j = i; j < a.Length; j++)
+                {
+                    if(i == j)
+                        continue;
+
+                    if (Math.Abs(a[i] - a[j]) == 2)
+                        counter++;
+                }
+            }
+            return counter;
+        }
+    }
+}
---
+++ HackerRankTests/Medium/PairsWithDiffTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HackerRank.Medium;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HackerRankTests.Medium
+{
+    [TestClass]
+    public class PairsWithDiffTests
+    {
+        [TestMethod]
+        public void TestGet()
+        {
+            var res = PairsWithDiff.Get(new[] {1, 5, 3, 4, 2}, 2);
+            Assert.AreEqual(3, res);
+        }
+    }
+}
