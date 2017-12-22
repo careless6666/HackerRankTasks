497fa36ecf5a7d694d751ba5338ec596df8aa7c5
alex usov <careless6666@gmail.com>
2017-12-22 16:06:42 +0300

    added first solution

---
+++ HackerRank/Hard/MaximumSubarraySum.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HackerRank.Hard
+{
+    //https://www.hackerrank.com/challenges/maximum-subarray-sum/problem
+
+    public class MaximumSubarraySum
+    {
+        private static long _max;
+
+        public static long CalcMax(long[] a, long m, long devider)
+        {
+            printCombination(a, a.Length, m, devider);
+
+            return _max;
+        }
+
+        public static long GetMax() => _max;
+
+        static void printCombination(long[] arr, long n, long r, long devider)
+        {
+            // A temporary array to store all combination one by one
+            var data = new long[r];
+
+            // Print all combination using temprary array 'data[]'
+            combinationUtil(arr, data, 0, n - 1, 0, r, devider);
+        }
+
+        /* arr[]  ---> Input Array
+           data[] ---> Temporary array to store current combination
+           start & end ---> Staring and Ending indexes in arr[]
+           index  ---> Current index in data[]
+           r ---> Size of a combination to be prlonged */
+        static void combinationUtil(long[] arr, long[] data, long start, long end,
+        long index, long r, long devider)
+        {
+            // Current combination is ready to be prlonged, print it
+            if (index == r)
+            {
+                long sum = 0;
+                for (long j = 0; j < r; j++)
+                {
+                    Console.Write(data[j] + " ");
+                    sum += data[j];
+                }
+
+                if (_max < sum % devider)
+                    _max = sum % devider;
+
+                sum = 0;
+
+                    Console.WriteLine();
+                return;
+            }
+
+            // replace index with all possible elements. The condition
+            // "end-i+1 >= r-index" makes sure that including one element
+            // at index will make a combination with remaining elements
+            // at remaining positions
+            for (long i = start; i <= end && end - i + 1 >= r - index; i++)
+            {
+                data[index] = arr[i];
+                combinationUtil(arr, data, i + 1, end, index + 1, r, devider);
+            }
+        }
+    }
+}
--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using HackerRank.Hard;
 
 namespace HackerRank
 {
@@ -7,9 +8,25 @@ namespace HackerRank
     {
         static void Main(string[] args)
         {
+            var divider = 7;
+            var repeat = 1;
+            var a = new long[] {3, 3, 9, 9, 5};
+
+            for(var i = 0; i < repeat; i++)
+                Repeat(divider, a);
+
+            Console.WriteLine(MaximumSubarraySum.GetMax());
+
+
             Console.ReadLine();
         }
 
+
+        private static void Repeat(long divider, long[] a)
+        {
+            for (int i = 0; i <= a.Length; i++)
+                MaximumSubarraySum.CalcMax(a, i, divider);
+        }
         
     }
 }
