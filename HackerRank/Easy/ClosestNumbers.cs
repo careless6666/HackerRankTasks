56968b6ac196aa927be79a2ed4ad6ee94a837a27
alex usov <careless6666@gmail.com>
2017-12-28 18:31:43 +0300

    closes numbers

---
+++ HackerRank/Easy/ClosestNumbers.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HackerRank.Easy
+{
+    public class ClosestNumbers
+    {
+        public static List<int> GetClosest(int[] a)
+        {
+            var l = a.ToList();
+            l.Sort();
+
+            var min = int.MaxValue;
+
+            var minList = new List<int>();
+
+            for (var i = 0; i < l.Count - 1; i++)
+            {
+                var val = l.ElementAt(i) - l.ElementAt(i + 1);
+                if (Math.Abs(val) <= min)
+                    min = Math.Abs(val);
+            }
+
+            for (var i = 0; i < l.Count - 1; i++)
+            {
+                var val = l.ElementAt(i) - l.ElementAt(i + 1);
+                if (Math.Abs(val) == min)
+                {
+                    minList.Add(l.ElementAt(i));
+                    minList.Add(l.ElementAt(i + 1));
+                }
+            }
+
+            return minList;
+        }
+    }
+}
---
+++ HackerRankTests/Easy/ClosestNumbersTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using HackerRank.Easy;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HackerRankTests.Easy
+{
+    [TestClass]
+    public class ClosestNumbersTests
+    {
+        [TestMethod]
+        public void TestGetClosest()
+        {
+            var res = ClosestNumbers.GetClosest(new[] {-20, -3916237, -357920, -3620601, 7374819, -7330761, 30, 6246457, -6461594, 266854});
+
+            Assert.IsTrue(new [] {-20, 30}.SequenceEqual(res));
+            
+            res = ClosestNumbers.GetClosest(new[] { -20, -3916237, -357920, -3620601, 7374819, -7330761, 30, 6246457, -6461594, 266854, -520, -470 });
+
+            Assert.IsTrue(new[] { -520, -470, -20, 30 }.SequenceEqual(res));
+
+            res = ClosestNumbers.GetClosest(new[] { 5, 4, 3, 2 });
+            
+            Assert.IsTrue(new[] { 2, 3, 3, 4, 4, 5 }.SequenceEqual(res));
+        }
+    }
+}
